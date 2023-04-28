4211f795368db4c0cf44a6635beb78bc40a3d576
jrobrien91 <obrienj@anl.gov>
2023-04-28 18:07:53 -0500

    ENH: working locally with use of startech serial to usb hub

--- app.py
+++ app.py
@@ -6,63 +6,39 @@ import logging
 from waggle.plugin import Plugin, get_timestamp
 
 def parse_values(sample, **kwargs):
-    # Note: Specific to WXT ASCII query commands
-    if sample.startswith(b'0R0'):
-        parms = ['Dm', 'Sm', 'Ta', 'Ua', 'Pa', 'Rc', 'Th', 'Vh']
-        data = parse.search("Dm={3D}D," +
-                            "Sm={.1F}M," +
-                            "Ta={.1F}C," +
-                            "Ua={.1F}P," +
-                            "Pa={.1F}H," +
-                            "Rc={.2F}M," +
-                            "Th={.1F}C," +
-                            "Vh={.1F}#" ,
-                            sample.decode('utf-8')
-                           )
-        # Can't figure out why I can't format parse class
-        strip = [float(var) for var in data]
-        ndict = dict(zip(parms, strip))
-
-    elif sample.startswith(b'0R2'):
-        parms = ['Ta', 'Ua', 'Pa']
-        data = parse.search("Ta={.1F}C," +
-                            "Ua={.1F}P," +
-                            "Pa={.1F}H" ,
+    # Note: The AQT ASCII data contains which specific variables are in the string
+    # as the second to last comma separated value within the line
+    print(sample)
+    if sample.startswith(b'20'):
+        data = parse.search("{ti}," +
+                            "{.1F}," +
+                            "{.1F}," +
+                            "{.1F}," +
+                            "{.3F}," +
+                            "{.3F}," +
+                            "{.3F}," +
+                            "{.3F}," +
+                            "{.1F}," +
+                            "{.1F}," +
+                            "{.1F}," +
+                            "{w}," +
+                            "{d}\r\n" ,
                             sample.decode('utf-8')
                             )
-        # Can't figure out why I can't format parse class
+        # Parse the variable names from the datastring
+        # Captured by the {w} flag
+        parms = data['w'].split(':')
+        # Convert the variables to floats
         strip = [float(var) for var in data]
+        # Create a dictionary to match the parameters and variables
         ndict = dict(zip(parms, strip))
-
-    elif sample.startswith(b'0R3'):
-        parms = ['Rc', 'Rd', 'Ri', 'Hc', 'Hd', 'Hi']
-        data = parse.search("Rc={.2F}M," +
-                            "Rd={.2F}s," +
-                            "Ri={.2F}M," +
-                            "Hc={.2F}M," +
-                            "Hd={.2F}s," +
-                            "Hi={.2F}M" ,
-                            sample.decode('utf-8')
-                            )
-        # Can't figure out why I can't format parse class
-        strip = [float(var) for var in data]
-        ndict = dict(zip(parms, strip))
-
-    elif sample.startswith(b'0R5'):
-        parms = ['Th', 'Vh', 'Vs', 'Vr']
-        data = parse.search("Th={.1F}C," +
-                            "Vh={.1F}N," +
-                            "Vs={.1F}V," +
-                            "Vr={.3F)}V" ,
-                            sample.decode('utf-8')
-                            )
-        # Can't figure out why I can't format parse class
-        strip = [float(var) for var in data]
-        ndict = dict(zip(parms, strip))
+        # Add the AQT datetime to the dictionary
+        ndict['datetime'] = data['ti']
+        ndict['uptime'] = int(data['d'])
 
     else:
         ndict = None
-                 
+
     return ndict
 
 
@@ -82,100 +58,118 @@ def start_publishing(args, plugin, dev, **kwargs):
     sched
     parse
     """
-    # Define the timestamp
-    timestamp = get_timestamp()
-    # Request Sample
-    logging.debug("send command to instrument to start poll")
-    # Note: WXT interface commands located within manual
-    dev.write(b'0R0\r\n')
+    # Note: AQT ASCII interface configuration described in manual
     line = dev.readline()
-    # Check for valid command
-    sample = parse_values(line) 
+    print('hey made it to start_publishing')
+    # Note: AQT has 1 min data output, need to check if bytes are returned
+    if len(line) > 0: 
+        # Define the timestamp
+        timestamp = get_timestamp()
+        logging.debug("Read transmitted data")
+        # Check for valid command
+        sample = parse_values(line) 
     
-    # If valid parsed values, send to publishing
-    if sample:
-        # setup and run publishing schedule
-        if kwargs['node_interval'] > 0:
-            # publish each value in sample
-            for name, key in kwargs['names'].items():
-                try:
-                    value = sample[key]
-                except KeyError:
-                    continue
-                # Update the log
-                if kwargs.get('debug', 'False'):
-                    print(timestamp, name, value, kwargs['units'][name], type(value))
-                logging.info("node publishing %s %s units %s type %s", name, value, kwargs['units'][name], str(type(value)))
-                plugin.publish(name,
-                               value=value,
-                               meta={"units" : kwargs['units'][name],
-                                     "sensor" : "vaisala-wxt536",
-                                     "missing" : "-9999.9"
-                                    },
-                               scope="node",
-                               timestamp=timestamp
-                              )
-                                    
-        if kwargs['beehive_interval'] > 0:
-            # publish each value in sample
-            for name, key in kwargs['names'].items():
-                try:
-                    value = sample[key]
-                except KeyError:
-                    continue
-                # Update the log
-                if kwargs.get('debug', 'False'):
-                    print(timestamp, name, value, kwargs['units'][name], type(value))
-                logging.info("beehive publishing %s %s units %s type %s", name, value, kwargs['units'][name], str(type(value)))
-                plugin.publish(name,
-                               value=value,
-                               meta={"units" : kwargs['units'][name],
-                                     "sensor" : "vaisala-wxt536",
-                                     "missing" : "-9999.9"
-                                    },
-                               scope="beehive",
-                               timestamp=timestamp
-                              )
+        # If valid parsed values, send to publishing
+        if sample:
+            # setup and publish to the node
+            if kwargs['node_interval'] > 0:
+                # publish each value in sample
+                for name, key in kwargs['names'].items():
+                    try:
+                        value = sample[key]
+                    except KeyError:
+                        continue
+                    # Update the log
+                    if kwargs.get('debug', 'False'):
+                        print(timestamp, name, value, kwargs['units'][name], type(value))
+                    print('node', timestamp, name, value, kwargs['units'][name], type(value))
+                    logging.info("node publishing %s %s units %s type %s", name, value, kwargs['units'][name], str(type(value)))
+                    plugin.publish(name,
+                                   value=value,
+                                   meta={"units" : kwargs['units'][name],
+                                         "sensor" : "vaisala-aqt530",
+                                         "missing" : '-9999.9',
+                                         "description" : kwargs['description'][name]
+                                         },
+                                   scope="node",
+                                   timestamp=timestamp
+                                   )
+            # setup and publish to the beehive                        
+            if kwargs['beehive_interval'] > 0:
+                # publish each value in sample
+                for name, key in kwargs['names'].items():
+                    try:
+                        value = sample[key]
+                    except KeyError:
+                        continue
+                    # Update the log
+                    if kwargs.get('debug', 'False'):
+                        print(timestamp, name, value, kwargs['units'][name], type(value))
+                    print('beehive', timestamp, name, value, kwargs['units'][name], type(value))
+                    logging.info("beehive publishing %s %s units %s type %s", name, value, kwargs['units'][name], str(type(value)))
+                    plugin.publish(name,
+                                   value=value,
+                                   meta={"units" : kwargs['units'][name],
+                                         "sensor" : "vaisala-aqt530",
+                                         "missing" : '-9999.9',
+                                         "description" : kwargs['description'][name]
+                                        },
+                                   scope="beehive",
+                                   timestamp=timestamp
+                                  )
 
 def main(args):
-    publish_names = {"wxt.wind.direction" : "Dm",
-                     "wxt.wind.speed" : "Sm",
-                     "wxt.env.temp" : "Ta",
-                     "wxt.env.humidity" : "Ua",
-                     "wxt.env.pressure" : "Pa",
-                     "wxt.rain.accumulation" : "Rc",
-                     "wxt.rain.duration" : "Rd",
-                     "wxt.rain.intensity" : "Ri",
-                     "wxt.rain.peak" : "Rp",
-                     "wxt.hail.accumulation" : "Hc",
-                     "wxt.hail.duration" : "Hd",
-                     "wxt.hail.intensity" : "Hi",
-                     "wxt.hail.peak" : "Hp",
-                     "wxt.heater.temp" : "Th",
-                     "wxt.heater.volt" : "Vh",
-                     "wxt.voltage.supply" : "Vs",
-                     "wxt.voltage.reference" : "Vr"
+    print('in main')
+    publish_names = {"aqt.env.temp" : "T",
+                     "aqt.env.pressure" : "P",
+                     "aqt.env.humidity" : "H",
+                     "aqt.gas.no2" : "NO2",
+                     "aqt.gas.co" : "CO",
+                     "aqt.gas.ozone" : "O3",
+                     "aqt.gas.no" : "NO",
+                     "aqt.gas.so2" : "SO2",
+                     "aqt.gas.h2s" : "H2S",
+                     "aqt.particle.pm1" : "PM1",
+                     "aqt.particle.pm2.5" : "PM2.5",
+                     "aqt.particle.pm10" : "PM10",
+                     "aqt.house.datetime" : "datetime",
+                     "aqt.house.uptime" : "uptime",
                     }
 
-    units = {"wxt.wind.direction" : "degrees",
-             "wxt.wind.speed" : "meters per second",
-             "wxt.env.temp" : "degree Celsius",
-             "wxt.env.humidity" : "percent",
-             "wxt.env.pressure" : "hectoPascal",
-             "wxt.rain.accumulation" : "milimeters",
-             "wxt.rain.duration" : "seconds",
-             "wxt.rain.intensity" : "millimeters per hour",
-             "wxt.rain.peak" : "millimeters per hour",
-             "wxt.hail.accumulation" : "hits per square centimeter",
-             "wxt.hail.duratioun" : "seconds",
-             "wxt.hail.intensity" : "hits per square centimeter per hour",
-             "wxt.hail.peak" : "hits per square centimeter per hour",
-             "wxt.voltage.supply" : "volts",
-             "wxt.heater.temp" : "degree Celsius",
-             "wxt.heater.volt" : "volts",
-             "wxt.voltage.reference" : "volts"
+    units = {"aqt.env.temp" : "degrees Celsius",
+             "aqt.env.pressure" : "hPa",
+             "aqt.env.humidity" : "percent relative humidity",
+             "aqt.gas.no2" : "ppm",
+             "aqt.gas.co" : "ppm",
+             "aqt.gas.ozone" : "ppm",
+             "aqt.gas.no" : "ppm",
+             "aqt.gas.so2" : "ppm",
+             "aqt.gas.h2s" : "ppm", 
+             "aqt.particle.pm1" : "microgram per cubic meter",
+             "aqt.particle.pm2.5" : "microgram per cubic meter",
+             "aqt.particle.pm10" : "microgram per cubic meter",
+             "aqt.house.datetime" : "UTC time",
+             "aqt.house.uptime" : "seconds"
              }
     
+    description = {"aqt.env.temp" : "Ambient Temperature",
+                   "aqt.env.pressure" : "Ambient Atmospheric Pressure",
+                   "aqt.env.humidity" : "Ambient Relative Humidity",
+                   "aqt.gas.no2" : "Nitrogen Dioxide Gas Concentration",
+                   "aqt.gas.co" : "Carbon Monoxide Gas Concentration",
+                   "aqt.gas.ozone" : "Ozone Gas Concentration",
+                   "aqt.gas.no" : "Nitric Oxide Gas Concentration",
+                   "aqt.gas.so2" : "Sulfur Dioxide Gas Concentration",
+                   "aqt.gas.h2s" : "Hydrogen Sulfide Gas Concentration", 
+                   "aqt.particle.pm1" : "Particulate Matter less than 1 microns in diameter",
+                   "aqt.particle.pm2.5" : "Particulate Matter less than 2.5 microns in diameter",
+                   "aqt.particle.pm10" : "Particulate Matter less than 10 microns in diameter",
+                   "aqt.house.datetime" : "UTC time in YYYY-MM-DDTHH:MM:SS format",
+                   "aqt.house.uptime" : "Time in seconds since instrument startup"
+                  }
+
+    print(args.device)
+    print(args.baud_rate)
     with Plugin() as plugin, serial.Serial(args.device, baudrate=args.baud_rate, timeout=1.0) as dev:
         while True:
             try:
@@ -185,7 +179,9 @@ def main(args):
                                  node_interval=args.node_interval,
                                  beehive_interval=args.beehive_interval,
                                  names=publish_names,
-                                 units=units)
+                                 units=units,
+                                 description=description
+                                 )
             except Exception as e:
                 print("keyboard interrupt")
                 print(e)
@@ -203,13 +199,13 @@ if __name__ == '__main__':
     parser.add_argument("--device",
                         type=str,
                         dest='device',
-                        default="/dev/ttyUSB1",
+                        default="/dev/ttyUSB3",
                         help="serial device to use"
                         )
     parser.add_argument("--baudrate",
                         type=int,
                         dest='baud_rate',
-                        default=19200,
+                        default=115200,
                         help="baudrate to use"
                         )
     parser.add_argument("--node-publish-interval",
@@ -220,7 +216,7 @@ if __name__ == '__main__':
                              "(negative values disable node publishing)"
                         )
     parser.add_argument("--beehive-publish-interval",
-                        default=1.0,
+                        default=0.0,
                         dest='beehive_interval',
                         type=float,
                         help="interval to publish data to beehive " +
